d3e24ee8071d15fbc9e41834ff602aef741da021
Stefan Brannfjell <braste10@nith.no>
2014-02-16 06:02:52 +0100

    Adding 2 folders in public_html: anguar, ember. Moved existing ember files into the ember folder.

---
+++ public_html/angular/routes.js
@@ -0,0 +1,39 @@
+/*
+	CONFIG
+*/
+function PageUrl(page) {
+	return "pages/" + page + ".html";
+}
+function ApiUrl(link) {
+	return "http://duots.dondaniello.com/duaac/" + link;
+}
+
+/*
+	ROUTES
+	(Routing all pages and hooking them to their controller)
+*/
+var app = angular.module('app', []).config(function($routeProvider) {
+	
+	$routeProvider.when('/', {
+		templateUrl: PageUrl('news'),
+		controller: 'NewsController'
+	});
+
+	$routeProvider.when('/home', {
+		templateUrl: PageUrl('news'),
+		controller: 'NewsController'
+	});
+
+	$routeProvider.when('/players/:id', {
+		templateUrl: PageUrl('profile'),
+		controller: 'ProfileController'
+	});
+
+	$routeProvider.when('/404', {
+		templateUrl: PageUrl('404')
+	});
+
+	$routeProvider.otherwise({
+		redirectTo : '/404'
+	});
+});
