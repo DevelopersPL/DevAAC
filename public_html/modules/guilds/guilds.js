911c7e0259443873bc33e205f576e74d6dfef7df
Stefan Brannfjell <braste10@nith.no>
2014-06-25 21:24:46 +0200

    Guild Module: View a guild. Players not done yet, waiting for API.

--- public_html/modules/guilds/guilds.js
+++ public_html/modules/guilds/guilds.js
@@ -1,8 +1,12 @@
 // Module Route(s)
 DevAAC.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/guilds', {
-        templateUrl: PageUrl('guilds'),
+        templateUrl: PageUrl('guilds/guilds'),
         controller: 'GuildsController'
+    });
+    $routeProvider.when('/guilds/:id', {
+        templateUrl: PageUrl('guilds/guild'),
+        controller: 'GuildController'
     });
 }]);
 
@@ -10,6 +14,22 @@ DevAAC.config(['$routeProvider', function($routeProvider) {
 DevAAC.controller('GuildsController', ['$scope', 'Guild',
     function($scope, Guild) {
         $scope.guilds = Guild.query({embed: 'owner'});
+    }
+]);
+DevAAC.controller('GuildController', ['$scope', 'Guild', '$routeParams', '$location', 
+    function($scope, Guild, $routeParams, $location) {
+        $scope.guild = false;
+        Guild.get({guildId: $routeParams.id}, function(guildInfo) {
+            $scope.guild = {
+                name: guildInfo.name,
+                created: guildInfo.creationdata,
+                motd: guildInfo.motd
+            };
+        }, function(response) {
+            if (response.status === 404) {
+                $location.path('/guilds');
+            }
+        });
     }
 ]);
 
