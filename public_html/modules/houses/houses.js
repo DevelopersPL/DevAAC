b803183d2feac5471fe0187971ffceed942b51d1
Stefan Brannfjell <braste10@nith.no>
2014-06-25 23:20:47 +0200

    Houses module: View house information.

--- public_html/modules/houses/houses.js
+++ public_html/modules/houses/houses.js
@@ -4,9 +4,39 @@ DevAAC.config(['$routeProvider', function($routeProvider) {
         templateUrl: PageUrl('houses'),
         controller: 'HousesController'
     });
+    $routeProvider.when('/houses/:id', {
+        templateUrl: PageUrl('houses/house'),
+        controller: 'HouseController'
+    });
 }]);
 
 // Module Controller(s)
+DevAAC.controller('HouseController', ['$scope', 'House', 'Player', '$routeParams', '$location',
+    function($scope, House, Player, $routeParams, $location) {
+        House.get({houseId: $routeParams.id}, function(house) {
+            $scope.house = house;
+            if (house.owner > 0) {
+                $scope.player = {
+                    name: 'Loading...'
+                }
+                Player.get({id:house.owner}, function(player) {
+                    $scope.player.name = player.name;
+                }, function(response) {
+                    if (response.status === 404) {
+                        $scope.player = {
+                            name: '[Deleted Player]'
+                        }
+                    }
+                });
+            }
+        }, function (response) {
+            if (response.status === 404) {
+                $location.path('/houses');
+            }
+        });
+    }
+]);
+
 DevAAC.controller('HousesController', ['$scope', 'House', 'Player',
     function($scope, House, Player) {
         $scope.houses = House.query(function(){
@@ -32,7 +62,7 @@ DevAAC.controller('HousesController', ['$scope', 'House', 'Player',
 // Module Factories(s)
 DevAAC.factory('House', ['$resource',
     function($resource){
-        return $resource(ApiUrl('houses/:guildId'), {}, {
+        return $resource(ApiUrl('houses/:houseId'), {}, {
             get: { cache: true },
             query: { isArray: true, cache: true }
         });
